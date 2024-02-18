b9dcd3cf3e3444deaeb71ff7d51d262f8a354ee1
venkatasairam26 <venkatasairam26@gmail.com>
2024-02-19 00:03:11 +0530

    added responsive to home page

---
+++ index.js
@@ -0,0 +1,8 @@
+let navListEle = document.getElementById('hamburger-button');
+let navListItemsEle = document.getElementById('nav-details')
+
+
+function onClickListIcon(){ 
+    navListItemsEle.classList.toggle("toggle-nav");
+    console.log(navListItemsEle)
+}
