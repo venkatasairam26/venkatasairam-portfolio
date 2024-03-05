d35bd3bac9f12190e89b1780ea6e12bb9a536603
venkatasairam26 <venkatasairam26@gmail.com>
2024-03-05 22:59:34 +0530

    build responsive and have to do some changes

--- index.js
+++
@@ -1,8 +0,0 @@
-let navListEle = document.getElementById('hamburger-button');
-let navListItemsEle = document.getElementById('nav-details')
-
-
-function onClickListIcon(){ 
-    navListItemsEle.classList.toggle("toggle-nav");
-    console.log(navListItemsEle)
-}
